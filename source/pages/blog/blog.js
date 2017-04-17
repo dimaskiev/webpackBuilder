b4eaf8d60bab94861c03209537fb440611b7665e
@romanchuk <manager.ua@gmail.com>
2017-04-17 15:08:34 +0300

    work

---
+++ source/pages/blog/blog.js
@@ -0,0 +1,5 @@
+import createMenu from '../../components/menu/menu';
+import './blog.scss';
+var menu = createMenu(['Главная', 'Блоггг'], 'menu');
+document.body.appendChild(menu);
+console.log('in blog.js')
